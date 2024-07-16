dedb0ec978a54bb15dc5f0d04b7e59348ffb2a60
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-16 12:26:33 +0600

    designed sidebar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
 
 const Root = () => {
+    const [openSidebar, setOpenSidebar] = useState(false);
+
     return (
         <main className="max-w-[1920px] h-screen mx-auto bg-takaBG bg-fixed bg-center bg-cover bg-no-repeat text-white overflow-x-hidden overflow-y-auto taka-scrollbar">
-            <Outlet />
+            <div className="flex items-start h-full justify-start gap-4">
+                <div className="h-screen">
+                    <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
+                </div>
+                <div className={`flex-1 mx-auto h-screen w-ful ${openSidebar ? 'overflow-x-hidden' : 'overflow-x-auto'}`}>
+                    <Outlet />
+                </div>
+            </div>
         </main>
     );
 };
