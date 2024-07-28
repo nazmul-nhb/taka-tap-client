e13aeabb25ea30c2084504bcf057bfe051a313ff
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-28 14:26:03 +0600

    created hook to handle all request; made cash in request functional

---
+++ src/hooks/useTransactionRequest.jsx
@@ -0,0 +1,50 @@
+import toast from "react-hot-toast";
+import useAxiosSecure from "./useAxiosSecure";
+import Swal from "sweetalert2";
+
+const useTransactionRequest = () => {
+	const axiosSecure = useAxiosSecure();
+
+	/**
+	 * Make a Transaction Request!
+	 *
+	 * @param {object} transactionInfo - Transaction info must have: { amount, agent, request_type, request_time, request_status }.
+	 */
+
+	const sendRequest = (transactionInfo) => {
+		axiosSecure
+			.post(`/transactions/request`, transactionInfo)
+			.then((res) => {
+				if (res?.data?.success) {
+					toast.success(res?.data?.message);
+				} else {
+					toast.error(res?.data?.message);
+					Swal.fire({
+						title: "Error!",
+						text: res?.data?.message,
+						icon: "error",
+						confirmButtonText: "Close",
+						color: "#fff",
+						background: "#f15d24ee",
+					});
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				if (error) {
+					Swal.fire({
+						title: "Error!",
+						text: error?.message,
+						icon: "error",
+						confirmButtonText: "Close",
+						color: "#fff",
+						background: "#f15d24ee",
+					});
+				}
+			});
+	};
+
+	return sendRequest;
+};
+
+export default useTransactionRequest;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import toast from "react-hot-toast";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { jwtDecode } from "jwt-decode";
 import { createContext, useEffect, useState } from "react";
 import useAxiosPublic from "../hooks/useAxiosPublic";
@@ -7,83 +7,102 @@ import useAxiosPublic from "../hooks/useAxiosPublic";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [userLoading, setUserLoading] = useState(true);
-    const axiosPublic = useAxiosPublic();
+	const [currentUser, setCurrentUser] = useState(null);
+	const [userLoading, setUserLoading] = useState(true);
+	const axiosPublic = useAxiosPublic();
 
-    // set user from token if already logged in
-    useEffect(() => {
-        const token = localStorage.getItem('taka-token');
-        if (token) {
-            const decodedUser = jwtDecode(token);
-            setCurrentUser(decodedUser);
-        }
-        setUserLoading(false);
-    }, []);
+	// set user from token if already logged in
+	useEffect(() => {
+		const token = localStorage.getItem("taka-token");
+		if (token) {
+			const decodedUser = jwtDecode(token);
+			setCurrentUser(decodedUser);
+		}
+		setUserLoading(false);
+	}, []);
 
-    // register function
-    const registerUser = async (user) => {
-        try {
-            const { data } = await axiosPublic.post(`/auth/register`, user);
-            return data;
-        } catch (error) {
-            console.error(error);
+	// register function
+	const registerUser = async (user) => {
+		try {
+			const { data } = await axiosPublic.post(`/auth/register`, user);
+			return data;
+		} catch (error) {
+			console.error(error);
 
-            if (error.response) {
-                return { success: false, message: error.response.data?.message || "Registration Failed!", status: error.response.status || 500 };
-            }
+			if (error.response) {
+				return {
+					success: false,
+					message: error.response.data?.message || "Registration Failed!",
+					status: error.response.status || 500,
+				};
+			}
 
-            // If the error doesn't have a response (e.g., network error)
-            return { success: false, message: error.response.data?.message || "Registration Error!", status: 500 };
-        }
-    };
+			// If the error doesn't have a response (e.g., network error)
+			return {
+				success: false,
+				message: error.response.data?.message || "Registration Error!",
+				status: 500,
+			};
+		}
+	};
 
-    // console.log(currentUser);
+	// console.log(currentUser);
 
-    const login = async (credential, pin) => {
-        try {
-            const { data } = await axiosPublic.post(`/auth/login`, { credential, pin });
-            if (data?.success) {
-                localStorage.setItem('taka-token', data?.token);
-                const decodedUser = jwtDecode(data?.token);
-                setCurrentUser(decodedUser);
-                return data;
-            } else {
-                return data;
-            }
-        } catch (error) {
-            console.error(error);
+	const login = async (credential, pin) => {
+		setUserLoading(true);
+		try {
+			const { data } = await axiosPublic.post(`/auth/login`, {
+				credential,
+				pin,
+			});
+			if (data?.success) {
+				localStorage.setItem("taka-token", data?.token);
+				const decodedUser = jwtDecode(data?.token);
+				setCurrentUser(decodedUser);
+				setUserLoading(false);
+				return data;
+			} else {
+				return data;
+			}
+		} catch (error) {
+			console.error(error);
+			setUserLoading(false);
+			if (error.response) {
+				return {
+					success: false,
+					message: error.response.data?.message || "Login Failed!",
+					status: error.response.status || 500,
+				};
+			}
 
-            if (error.response) {
-                return { success: false, message: error.response.data?.message || "Login Failed!", status: error.response.status || 500 };
-            }
+			// If the error doesn't have a response (e.g., network error)
+			return {
+				success: false,
+				message: error.response.data?.message || "Login Error!",
+				status: 500,
+			};
+		}
+	};
 
-            // If the error doesn't have a response (e.g., network error)
-            return { success: false, message: error.response.data?.message || "Login Error!", status: 500 };
-        }
-    };
+	const logOut = () => {
+		if (currentUser) {
+			localStorage.removeItem("taka-token");
+			setCurrentUser(null);
+			toast.success("Logged Out Successfully!");
+		} else {
+			toast.error("No User Logged In!");
+		}
+	};
 
-    const logOut = () => {
-        if (currentUser) {
-            localStorage.removeItem('taka-token');
-            setCurrentUser(null);
-            toast.success("Logged Out Successfully!");
-        } else {
-            toast.error("No User Logged In!");
-        }
-    };
+	const authInfo = { currentUser, userLoading, registerUser, login, logOut };
 
-    const authInfo = { currentUser, userLoading, registerUser, login, logOut };
-
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+	return (
+		<AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+	);
 };
 
 AuthProvider.propTypes = {
-    children: PropTypes.node
-}
+	children: PropTypes.node,
+};
 
 export default AuthProvider;
