cae099106623b6ee71324641e9a542d50db42dc9
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-25 12:09:57 +0600

    updated baseURL dynamically for development and production

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
-})
+export const baseURL = import.meta.env.VITE_API_URL;
+
+const axiosPublic = axios.create({ baseURL });
 
 const useAxiosPublic = () => {
-    return axiosPublic;
+	return axiosPublic;
 };
 
 export default useAxiosPublic;
